27ed357ac8cd4104ceb3c6329b9073d84cafa95f
YiHuan <linyc280@gmai.com>
2024-09-23 15:19:04 +0800

    feat:新增註冊/登入/儲存api

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const mysql = require("mysql")
@@ -5,6 +6,7 @@ const cors = require("cors")
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
@@ -39,18 +41,110 @@ app.post("/register", async (req, res) => {
         if (results.length > 0) {
             return res.json("帳號已存在");
         }
-    
-    const values = [nickName, account, password];
-    db.query(sql, values, (err, data) => {
 
+        const values = [nickName, account, password];
+        db.query(sql, values, (err, data) => {
+
+            if (err) {
+                return res.json("Error")
+            } else {
+                return res.json(data)
+            }
+        })
+    })
+});
+//登入
+app.post('/login', async (req, res) => {
+    const { account, password } = req.body;
+    const sql = "SELECT nickName,account, password,record FROM member WHERE account = ?"
+    db.query(sql, [account], (err, data) => {
+        if (err) {
+            console.log(err)
+            return res.json("Error")
+        } else if (!data.length) {
+            return res.json("incorrectAccount")
+        } else if (data[0].password !== password) {
+            return res.json("incorrectPassword")
+        }
+        else {
+            const token = jwt.sign({ account: account }, process.env.JWT_SECRET, { expiresIn: '1h' });
+            const nickName = data[0].nickName
+            const record = data[0].record !== "" ? JSON.parse(data[0].record) : []
+            console.log('record', record)
+            return res.json({
+                token: token,
+                account: account,
+                nickName: nickName,
+                record: record
+            })
+        }
+    })
+
+});
+//儲存
+app.post('/saveCard', async (req, res) => {
+    const { account, historyList, type } = req.body;
+    const token = req.headers.authorization?.split(' ')[1];
+    
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided.' });
+    }
+    // 驗證 token
+    const decoded = verifyToken(token);    
+
+    if (!decoded || typeof decoded === 'object' && decoded.error) {
+        if (decoded && decoded.error === 'Token expired') {
+            return res.status(401).json({ message: 'Token has expired.' });
+        }
+        return res.status(401).json({ message: 'Invalid token.' });
+    }
+
+    // 如果 token 有效，比對 account
+    const decodedAccount = decoded.account;
+    if (decodedAccount !== account) {
+        return res.status(401).json({ message: 'Invalid token.' });
+    }
+    
+
+    db.query('SELECT record FROM member WHERE account = ?', [account], (err, results) => {
         if (err) {
             return res.json("Error")
-        } else {
-            return res.json(data)
+        }
+        if (results.length) {
+            const oldRecord = results[0].record !== "" ? JSON.parse(results[0].record) : []
+            const newItem = {
+                "historyList": historyList,
+                "type": type
+            }
+            oldRecord.push(newItem)
+            const newRecord = JSON.stringify(oldRecord)
+            db.query('UPDATE member set record= ? WHERE account = ?', [newRecord, account], (err, results) => {
+                if (err) {
+                    return res.json("Error")
+                } else {
+                    return res.json(oldRecord)
+                }
+
+            })
+
         }
     })
 })
-});
+//驗證token
+function verifyToken(token) {
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        // 檢查過期時間
+        if (decoded.exp * 1000 <= Date.now()) {
+            return { error: 'Token expired' };
+        }
+        return decoded;
+    } catch (error) {
+        return null;
+    }
+}
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
